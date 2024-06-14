103effa4b92b40a043fd871f4719ed8319a27cea
s1280005 <s1280005@u-aizu.ac.jp>
2024-06-14 14:47:44 +0900

    Stations 15 is finished

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,42 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+
+  await delay(3000);
+  return userList.map(buildFullName);
+
+  // value = await promise; は "promise.then(value => {}); 扱い"
+  // const result = await test();
+  // return await result;
 }
 
-function test() {
-  return
+// function test() {
+//   return;
+// }
+
+function delay(ms) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, ms);
+  });
 }
 
+function buildFullName(data) {
+  const full_name = data.family_name + " " + data.first_name;
+  data["full_name"] = full_name;
+  return data;
+}
